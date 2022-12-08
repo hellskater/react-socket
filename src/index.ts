9d41d7d34c11388d9c99cc0f64d904ae54c32cff
mukun_poudel <mukun@herm.is>
2022-12-08 22:32:30 +0530

    initial commit '

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
-import { useState } from 'react';
-
+import { useState, useEffect } from 'react';
 export const useStateFul = () => {
   const [state, setState] = useState(false);
   return [state, setState];
+};
+
+export const useEffectFul = () => {
+  return useEffect(() => {});
 };
